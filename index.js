5a8b52769308ee6ec2953d814f33d455e8322f84
Tomtom-dev <thomasheddadji0@gmail.com>
2020-12-16 16:17:49 +0100

    add function to move the group of space invader

--- index.js
+++ index.js
@@ -37,6 +37,59 @@ document.addEventListener('DOMContentLoaded', () => {
         }
         squares[currentShooterIndex].classList.add('shooter')
     }
-    
+
+    // move the invaders
+
+    function moveInvaders(){
+        const leftEdge = alienInvaders[0] % width === 0
+        const rightEdge= alienInvaders[alienInvaders.length-1] % width === width-1 
+
+        if((leftEdge && direction === -1 ) || (rightEdge && direction === 1 )){
+            direction= width
+        }else if(direction === width){
+            if(leftEdge) direction =1
+            else direction= -1
+        }
+        for( let i=0; i <= alienInvaders.length-1; i++){
+            squares[alienInvaders[i]].classList.remove('invader')
+        }
+        for( let i=0; i <= alienInvaders.length-1; i++){
+            alienInvaders[i] += direction
+        }
+        for( let i=0; i <= alienInvaders.length-1; i++){
+            squares[alienInvaders[i]].classList.add('invader')
+        }
+
+        // game is over
+
+        if(squares[currentShooterIndex].classList.contains('invader', 'shooter')){
+            resultDisplay.textContent = 'Game Over'
+            squares[currentShooterIndex].classList.add('boom')
+            clearInterval(invaderId)
+        }
+
+        for (let i=0; i <= alienInvaders.length-1; i++){
+            if(alienInvaders[i] > (squares.length - (width-1) )){
+                resultDisplay.textContent='Game Over'
+                clearInterval(invaderId)
+            }
+        }
+    }
+
+    // shoot the alien
+
+    function shoot (e){
+        let laserId
+        let currentLaserIndex = currentShooterIndex
+
+        //move the laser to the alien invader
+
+        function moveLaser (){
+
+        }
+    }
+
+
+    invaderId = setInterval(moveInvaders, 500)
     document.addEventListener('keydown', moveShooter)
 })
